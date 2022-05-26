09e42912ef67aa99849184384b4bdb09ee20598f
Sindre S. Kjær <sindre@sindrekjr.no>
2022-05-26 02:50:03 +0200

    Created sh/ps1.sh

--- .bashrc
+++ .bashrc
@@ -1,21 +1,6 @@
 ## custom stuff
 source $HOME/sh/auto-ssh-agent.sh
-source $HOME/sh/kube-ps1.sh
-
-# aliases
-alias gitdir='cd $GITDIR'
-
-# PS1
-case "$TERM" in
-    xterm-color|*-256color) colours=yay;;
-esac
-
-if [ "$colours" = yay ]; then
-    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\] \[\033[33m\]\w\[\033[36m\]`__git_ps1`\[\033[00m\] `kube_ps1`\n$ '
-else
-    PS1='${debian_chroot:+($debian_chroot)}\u@\h \w `__git_ps1` `kube_ps1`\n$ '
-fi
-unset colours
+source $HOME/sh/ps1.sh
 
 # set GPG_TTY for gpgsigning in wsl shell
 export GPG_TTY=$(tty)
---
+++ sh/ps1.sh
@@ -0,0 +1,33 @@
+if [ -d "$HOME/kube-ps1/kube-ps1.sh" ] ; then
+    source $HOME/kube-ps1/kube-ps1.sh
+    kube='`kube_ps1`'
+fi
+
+case "$TERM" in
+    xterm-color|*-256color) colours=yay;;
+esac
+
+## build PS1
+PS1='${debian_chroot:+($debian_chroot)}'
+
+# directory
+if [ "$colours" = yay ] ; then
+    PS1="$PS1\[\033[01;32m\]\u@\h\[\033[00m\] \[\033[33m\]\w\[\033[36m\]"
+else
+    PS1="$PS1\u@\h \w"
+fi
+
+# git
+git='`__git_ps1`'
+PS1="$PS1$git"
+if [ "$colours" = yay ] ; then
+    PS1="$PS1\[\033[00m\]"
+fi
+
+# kube
+PS1="$PS1 $kube"
+
+# prompt
+PS1="$PS1\n$ "
+
+unset colours
